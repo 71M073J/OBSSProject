e28767ad97f54c018617a54c043a0bea5660badb
Timotej Ciglarič <ciglarictimotej@gmail.com>
2022-11-15 15:03:17 +0100

    optimiziraj še smh smh

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import collections
 import math
 
 from scipy import ndimage
@@ -7,9 +8,13 @@ import numpy as np
 import numpy.lib.stride_tricks as stt
 import matplotlib.pyplot as plt
 import time
+
 fs = 250
-rec = wfdb.rdrecord("./long-term-st-database-1.0.0/s20011")
-sig = rec.p_signal[:, 0][0:1250]#0:250 * 60 * 60]
+filename = "./long-term-st-database-1.0.0/s20011"
+anns = wfdb.rdann(filename, "atr")
+true_beats = anns.sample
+rec = wfdb.rdrecord(filename)
+sig = rec.p_signal[:, 0][:]#0:250 * 60 * 60]
 
 b1 = 0.025 * fs
 b2 = 0.06 * fs
@@ -28,40 +33,38 @@ impulse[len(filt)//2] = 1
 test = 6 * fs * ndimage.gaussian_laplace(-impulse, b1) # TESTNI FILTER ZA LoG
 #plt.plot(range(-len(filt)//2,len(filt)//2,1),test)
 #plt.show()
-
+#test = filt#TODO a bomo LoG uporablal al ne
 #r_peaks = np.convolve(sig, filt)
 r_peaks = np.convolve(sig, test, mode="same")
 #plt.plot(r_peaks)
 #plt.plot(r_peaks)
 #plt.plot(r_peaks2)
 #plt.show()
-r_candidates = np.full(len(r_peaks), True)
-left = (np.diff(r_peaks) >= 0)
-right = (np.diff(r_peaks[::-1])[::-1] >= 0)
-r_candidates[:-1] = r_candidates[:-1] & right
-r_candidates[1:] = r_candidates[1:] & left
-r_candidates = np.arange(len(r_peaks))[r_candidates]
+#r_candidates = np.full(len(r_peaks), True)
+#left = (np.diff(r_peaks) >= 0)
+#right = (np.diff(r_peaks[::-1])[::-1] >= 0)
+#r_candidates[:-1] = r_candidates[:-1] & right
+#r_candidates[1:] = r_candidates[1:] & left
+#r_candidates = np.arange(len(r_peaks))[r_candidates]
 
 second_order_diff = np.convolve(sig,np.array((-1, 2, -1)), mode="same")
 c1 = 0.55
 x2n = second_order_diff
 sn = r_peaks * (sig + c1 * x2n)
-plt.plot(sn, c="red")
-plt.plot(x2n, c="green")
-plt.plot(sig, c="magenta")
+#plt.plot(sn, c="red")
+#plt.plot(x2n, c="green")
+#plt.plot(sig, c="magenta")
 print(len(sn))
 cands = []
-#TODO OPTIMIZE THIS WIHT NUMPY
-start_t = time.time()
+
 maxl = len(sig) - 1
 r = int(np.floor(fs * 0.2 + 0.5))
 np.arange(len(sn))
 
-#TODO mogoče loop do r da zračunamo vse offsete?
-cands2 =[]
 #np.abs(sn) < np.abs(sn - )
 #np.where()
-f = (np.abs(sn[r:-r]) < stt.sliding_window_view(np.abs(sn), 2 * r + 1).T).sum(axis=0)
+abssn = np.abs(sn)
+f = (np.abs(sn[r:-r]) < stt.sliding_window_view(abssn, 2 * r + 1).T).sum(axis=0)
 cands = np.arange(r, len(f) + r)[f == 0]
 #f = f.sum(axis=0)
 #css = [[] for i in sn]
@@ -74,16 +77,71 @@ cands = np.arange(r, len(f) + r)[f == 0]
 #                               np.clip(int(k - r), a_min=0, a_max=maxl):
 #                               np.clip(int(k + r), a_min=0, a_max=maxl)]))
 
-asadasdasda = 0
+start_t = time.time()
+T = 0.6
+# np.argpartition([...])#maybe celo hitreje da gremo čez sn namesto čez kandidate
+#s5 = np.argpartition(stt.sliding_window_view(np.abs(sn), 10 * fs), -5,axis=1)[:,-5:]
+#NVM this array is 384 GiB ()
+w1 = np.zeros(len(cands))
+w2 = np.zeros(len(cands))
+taus = np.arange(1,6).astype(np.float32)[::-1]
+taus /= np.sum(taus)
+for ind, i in enumerate(cands):#navadn loop bo še najhitrej
+    small = False
+    curind = i
+    idx = ind
+    max5 = np.zeros(5)
+    minind = 0
+    while idx > -1 and (curind:=cands[idx]) + 10 * fs > i:
+        #curind = cands[idx]
+        curr = abssn[curind]
+        if ind - idx < 5:
+            minind = np.argmin(max5)
+        else:
+            minind = np.nonzero(max5)[0][np.argmin(max5[np.nonzero(max5)])]
+        if curr > max5[minind]:
+            max5[minind] = curr
+        #get max 5
+        idx -= 1
+    w1[ind] = max5[np.nonzero(max5)[0][np.argmin(max5[np.nonzero(max5)])]]
 
+    ms = np.zeros(7)
+    mind = np.clip(ind-6, a_min=0, a_max=ind)
+    ms[1:ind - mind + 1] = cands[mind:ind][::-1]
+    ms[0] = i
+    difms = np.diff(ms[::-1])[::-1]
+    if difms[1:].nonzero()[0].any() and difms[0] < np.mean(difms[1:][difms[1:].nonzero()]) * 0.7:
+        difms[0:5] = difms[1:6]
+        small = True
+    num_nonzero = difms[:-1].nonzero()[0].shape[0]
+    ie = np.sum((taus[:num_nonzero] / np.sum(taus[:num_nonzero])) * difms[:num_nonzero])
+    beta1 = 1
+    beta2 = 1
+    if small:
+        w2[ind] = beta1 + beta2 * np.abs(((ms[0] - ms[2])/2) / ie - np.floor(0.5 + (ms[0] - ms[1]) / ie))
+    else:
+        w2[ind] = beta1 + beta2 * np.abs((ms[0] - ms[1])/ie - np.floor(0.5 + (ms[0] - ms[1])/ie))
+
+
+w1 *= T
+
+adaptive_threshold = w1 * w2
+adapted_cands = cands[adaptive_threshold < np.abs(sn[cands])]
 end_t = time.time() + 1e-6
+
+print(adaptive_threshold, np.abs(sn[cands]))
+print(len(cands), "dolžina kandidatov")
 print(f"time elapsed for {len(sig)/fs} sec: ", end_t - start_t, f" speed {(len(sig)/fs)/(end_t - start_t)}s/s")
+print(len(true_beats), len(adapted_cands), "KEKW")
+#plt.plot(cands, adaptive_threshold, c="red")
+#plt.plot(sig)
+#plt.show()
 #TODO baseline extraction??? kaj je to, what do i do
 #TODO MORE PREPROCESSING - we require accurate candidates
 #print(r_candidates, cands)
 #final = r_candidates
 candidates = np.arange(len(sig))[cands]
-final = set(r_candidates).intersection(set(cands))
+#final = set(r_candidates).intersection(set(cands))
 final = cands
 #final = [r_candidates[0]]
 #for i in range(1, len(r_candidates)):
@@ -95,37 +153,13 @@ final = cands
 #print(np.diff(np.diff(sig)))
 #final = cands + r + 1
 #print(final)
+plt.figure(figsize=(15,5))
 plt.plot(r_peaks)
 #plt.plot(second_order_diff)
 #plt.plot(sig)
 for i in final:
-    ...
     plt.axvline(x=i, c="red")
+for i in adapted_cands:
+    print(i)
+    plt.axvline(x=i, c="blue")
 plt.show()
-
-
-
-
-
-
-
-quit()
-#filter = np.cos(np.ones(20)/20 * 2 * np.pi)
-#filter /= np.sum(filter)
-#sig = np.convolve(sig, filter)
-#fs = 250
-print("pre-fft")
-wave = np.fft.fft(sig)
-print(("post-fft"))
-#plt.plot(np.arange(len(wave)), np.real(wave))
-#plt.plot(np.fft.fftfreq(wave.size, 1 / fs), wave.real)
-# plt.plot(range(len(wave)), np.angle(wave), c="orange")
-#plt.plot(range(len(wave)), np.imag(wave), c="red", alpha=0.3)
-#plt.xlim(0)
-print("post-plot")
-#plt.show()
-# print(vars(rec))
-#print()
-# wfdb.rdrecord()
-# BRUH
-# wtf is dduis
