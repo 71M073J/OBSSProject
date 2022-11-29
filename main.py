2cd8aa704554685233154a552efa132bfca34204
71M073J <tc7476@student.uni-lj.si>
2022-11-29 19:58:36 +0100

    Just report to do now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
 import math
+import os
 
 import matplotlib.animation as animation
 from scipy import ndimage
@@ -10,33 +11,40 @@ import numpy.lib.stride_tricks as stt
 import matplotlib.pyplot as plt
 import time
 from scipy.signal import firwin, firwin2, freqz
+from scipy.optimize import minimize
 import json
 
 
-def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, name="default",
-           maxbeta=2.0, minT=0.5, maxT=1.0, signum=0, annotate=False):
-    #print(len(sig))
-    #TODO BASELINE EXTRACTION
+def detect(sig, true_beats, signum, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, name="default",
+           annotate=False, verbose=True, plot=False, Tlim=(5.0, 5.0), beta1lim=(0.0, 1.0), beta2lim=(0.0, 1.0)):
+    # print(len(sig))
     # HIGH_PASS FILTER
     # FIR FILTER
     # cutoff okoli 0.8 Hz
     t0 = time.time()
     cutoff_Hz = 0.8
     nyq_freq = fs / 2
-    fir_filter_highpass08 = firwin(1001, cutoff_Hz/nyq_freq, pass_zero="highpass")
-    #plt.plot(sig)
+    fir_filter_highpass08 = firwin(1001, cutoff_Hz / nyq_freq, pass_zero="highpass")
+    if plot and len(sig) > fs * 60 * 5:
+        print("Will not plot over 5 min of data; setting plot to false.")
+        # plot = False
+    if plot:
+        plt.plot(sig, c="purple")
     sig = np.convolve(sig, fir_filter_highpass08, mode="same")
     sig -= np.mean(sig)
-    #plt.show()
-    print(f"Baseline Extraction duration:{time.time() - t0} seconds")
-    #quit()
+    sig /= np.abs(sig).max()
+    if plot:
+        plt.plot(sig, c="red")
+    if verbose:
+        print(f"Baseline Extraction duration:{time.time() - t0} seconds")
+    # quit()
     t1 = time.time()
     b1 = 0.025 * fs
     b2 = 0.06 * fs
     c = 2 * (b2 - b1) / (2 * b1 + 1)
     filt_size = 51
     filt = np.zeros(filt_size)
-    #print(b1, b2, c)
+    # print(b1, b2, c)
     for i in range(-len(filt) // 2, len(filt) // 2, 1):
         if -b1 < i < b1:
             filt[i + len(filt) // 2] = c
@@ -51,7 +59,8 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
     # test = filt#TODO a bomo LoG uporablal al ne
     # r_peaks = np.convolve(sig, filt)
     r_peaks = np.convolve(sig, test, mode="same")
-    print(f"LoG filtering duration: {time.time() - t1} seconds.")
+    if verbose:
+        print(f"LoG filtering duration: {time.time() - t1} seconds.")
     # plt.plot(r_peaks)
     # plt.plot(r_peaks)
     # plt.plot(r_peaks2)
@@ -77,11 +86,11 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
     r = int(np.floor(fs * 0.2 + 0.5))
     np.arange(len(sn))
 
-
     abssn = np.abs(sn)
     f = (np.abs(sn[r:-r]) < stt.sliding_window_view(abssn, 2 * r + 1).T).sum(axis=0)
     cands = np.arange(r, len(f) + r)[f == 0]
-    print(f"Candidate filtering 1 duration: {time.time() - start_t} seconds.")
+    if verbose:
+        print(f"Candidate filtering 1 duration: {time.time() - start_t} seconds.")
     # f = f.sum(axis=0)
     # css = [[] for i in sn]
     # for k, i in enumerate(sn):
@@ -100,12 +109,13 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
     w2 = np.zeros(len(cands))
     taus = np.arange(1, 6).astype(np.float32)[::-1]
     taus /= np.sum(taus)
+    maxs = 0
     for ind, i in enumerate(cands):  # navadn loop bo še najhitrej
         small = False
-        #curind = i
+        # curind = i
         idx = ind
         max5 = np.zeros(5)
-        #minind = 0
+        # minind = 0
         while idx > -1 and (curind := cands[idx]) + 10 * fs > i:
             # curind = cands[idx]
             curr = abssn[curind]
@@ -133,7 +143,8 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
             w2[ind] = np.abs(((ms[0] - ms[2]) / 2) / ie - np.floor(0.5 + (ms[0] - ms[1]) / ie))
         else:
             w2[ind] = np.abs((ms[0] - ms[1]) / ie - np.floor(0.5 + (ms[0] - ms[1]) / ie))
-    print(f"Adaptive threshold filtering duration: {time.time() - t3} seconds.")
+    if verbose:
+        print(f"Adaptive threshold filtering duration: {time.time() - t3} seconds.")
     if search:
         w1save = np.copy(w1)
         w2save = np.copy(w2)
@@ -141,81 +152,62 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
         maxscore = len(true_beats)
         true = set(true_beats)
         maxs = 0
-        maxvals = 0
-        a = np.arange(0.0, 0.4, 0.05)
-        b = np.arange(0., maxbeta, 0.25)
-        c = np.arange(0., maxbeta, 0.25)
-        ims = np.zeros((len(a), len(b), len(c)))
-        t_s = time.time()
-        maxvalslist = []
-        for i1, T in enumerate(a):
-            for i2, beta1 in enumerate(b):
-                for i3, beta2 in enumerate(c):
-                    w1 = w1save * T
-                    w2 = beta1 + beta2 * w2save
-                    adaptive_threshold = w1 * w2
-                    adapted_cands = cands[adaptive_threshold < np.abs(sn[cands])]
-                    # scoring:
-                    tp = 0
-                    fp = 0
-                    fn = 0
-                    cand = set(adapted_cands)
-                    for can in cand:
-                        gotOne = False
-                        for offset in range(-5,6,1):
-                            if can + offset in true:
-                                gotOne = True
-                                break
-                        if gotOne:
-                            tp += 1
-                        else:
-                            fp += 1
-                    for tr in true:
-                        gotOne = False
-                        for offset in range(-5, 6, 1):
-                            if tr + offset in cand:
-                                gotOne = True
-                                break
-                        if not gotOne:
-                            fn += 1
-                    print(f"True Positive: {tp/maxscore}, \nFalse Positive:"
-                          f" {fp/maxscore} \nFalse Negative: {fn/maxscore}")
-                    print("params", T, beta1, beta2,"maxs", maxs, len(adapted_cands), maxscore)
-                    #if tp/(fp + 1) > maxs:
-                    #    maxs = tp/(fp + 1)
-                    #    maxvals = (T, beta1, beta2)
-                    fs = tp / (tp + 0.5 * (fp + fn))
-                    ims[i1, i2, i3] = fs
-                    print("F-score:", fs)
-                    maxvalslist.append((fs,(T, beta1, beta2)))
-                    if fs > maxs:
-                        maxs = fs
-                        maxvals = (T, beta1, beta2)
-        maxvalslist = [x[1] for x in maxvalslist if x[0] == maxs]
-        print(maxs, maxvals)
-        print(f"Search duration: {time.time() - t_s} seconds.")
-        with open("heartbeat_search.json", "rb") as f:
-            dicc = json.load(f)
-            dicc[name + str(signum)] = maxvals
-            dicc[name + str(signum) + "f_score"] = maxs
-            dicc[name + str(signum) + "maxvals"] = maxvalslist
-        with open("heartbeat_search.json", "w") as f:
-            json.dump(dicc, f)
-        fig, ax = plt.subplots()
-        anims = []
-        for h in ims:
-            im = ax.imshow(h, animated=True, vmin=ims.min(), vmax=ims.max())
-            anims.append([im])
-        ani = animation.ArtistAnimation(fig, anims, interval=500, blit=True,
-                                        repeat_delay=1000)
-        ani.save(f"./{name}{signum}.mp4")
-        #plt.show()
+
+        def eval_params(x):
+            T, beta1, beta2 = x
+            w1 = w1save * T
+            w2 = beta1 + beta2 * w2save
+            adaptive_threshold = w1 * w2
+            adapted_cands = cands[adaptive_threshold < np.abs(sn[cands])]
+            # scoring:
+            tp = 0
+            fp = 0
+            fn = 0
+            cand = set(adapted_cands)
+            for can in cand:
+                gotOne = False
+                for offset in range(-5, 6, 1):
+                    if can + offset in true:
+                        gotOne = True
+                        break
+                if gotOne:
+                    tp += 1
+                else:
+                    fp += 1
+            for tr in true:
+                gotOne = False
+                for offset in range(-5, 6, 1):
+                    if tr + offset in cand:
+                        gotOne = True
+                        break
+                if not gotOne:
+                    fn += 1
+
+            # if tp/(fp + 1) > maxs:
+            #    maxs = tp/(fp + 1)
+            #    maxvals = (T, beta1, beta2)
+            fs = tp / (tp + 0.5 * (fp + fn))
+            # if verbose:
+            sensitivity = tp / (tp + fn + 1e-12)
+            pos_pred = tp / (tp + fp + 1e-12)
+            score = (sensitivity + pos_pred)
+            print("Score:", score / 2)
+            return -fs
+            # ims[i1, i2, i3] = fs
+            # print("F-score:", fs)
+            # maxvalslist.append((fs, (T, beta1, beta2)))
+
+        res = minimize(eval_params, np.array((T, beta1, beta2)), method="Nelder-Mead",
+                       bounds=[Tlim, beta1lim, beta2lim])
+        print(res.x)
+        with open("min_params.txt", "a") as fa:
+            fa.write(str(res.x) + ", F-score: " + str(res.fun))
     else:
+        print(np.mean(sn[cands]))
         w1 *= T
         w2 = beta1 + beta2 * w2
         adaptive_threshold = w1 * w2
         adapted_cands = cands[adaptive_threshold < np.abs(sn[cands])]
-        end_t = time.time() + 1e-6
         cand = set(adapted_cands)
         tp = 0
         fp = 0
@@ -225,7 +217,7 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
         maxs = 0
         for can in cand:
             gotOne = False
-            for offset in range(-5, 6, 1):
+            for offset in range(-int(fs * 0.15 / 2), int(fs * 0.15 / 2) + 1, 1):
                 if can + offset in true:
                     gotOne = True
                     break
@@ -235,63 +227,79 @@ def detect(sig, true_beats, T=0.7, beta1=1.0, beta2=1.0, fs=250, search=False, n
                 fp += 1
         for tr in true:
             gotOne = False
-            for offset in range(-5, 6, 1):
+            for offset in range(-int(fs * 0.15 / 2), int(fs * 0.15 / 2) + 1, 1):
                 if tr + offset in cand:
                     gotOne = True
                     break
             if not gotOne:
                 fn += 1
-        print(f"True Positive: {tp / maxscore}, \nFalse Positive:"
-              f" {fp / maxscore} \nFalse Negative: {fn / maxscore}")
-        print("params", T, beta1, beta2, "maxs", maxs, len(adapted_cands), maxscore)
-        fs = tp / (tp + 0.5 * (fp + fn))
-        print("F-score:", fs)
-        print(f"time elapsed for {len(sig) / fs} sec: ", end_t - t0,
-              f" speed {(len(sig) / fs) / (end_t - start_t)}s/s")
-        sensitivity = tp / (tp + fn)
-        pos_pred = tp / (tp + fp)
-        if annotate:
-            with open("det/" + name + ".det", "w") as f:
-                for i in adapted_cands:
-                    f.write(i)
 
-    #print(adaptive_threshold, np.abs(sn[cands]))
-    #print(len(cands), "dolžina kandidatov,")
-    #print(len(true_beats), len(adapted_cands), "resničnih QRS in pa končnih kandidatov")
+        end_t = time.time() + 1e-6
+        if verbose:
+            print(f"True Positive: {tp / maxscore}, \nFalse Positive:"
+                  f" {fp / maxscore} \nFalse Negative: {fn / maxscore}")
+            print("params", T, beta1, beta2, "maxs", maxs, len(adapted_cands), maxscore)
+        f_s = tp / (tp + 0.5 * (fp + fn))
+        print("F-score:", f_s)
+        print(f"time elapsed for {len(sig) / fs} sec: ", end_t - t0,
+              f"sec, speed {(len(sig) / fs) / (end_t - start_t)} s/s")
+        sensitivity = tp / (tp + fn + 1e-12)
+        pos_pred = tp / (tp + fp + 1e-12)
+        print(f"Sensitivity: {sensitivity}\n+Predictivity: {pos_pred}")
+        if plot:
+            for x in adapted_cands:
+                plt.axvline(x, alpha=0.5)
+            plt.show()
+        if annotate:
+            if not os.path.exists("./det"):
+                os.mkdir("./det")
+            with open(f"det/{name}_signal{signum}.det", "w") as f:
+                data = "\n".join([f"0:00:00.00 {x} N 0 0 0" for x in adapted_cands])
+                f.write(data)
+        return sensitivity, pos_pred
 
 
 if __name__ == "__main__":
+    # TODO use minimiser instead of search
     fs = 250
-    for h in range(1,10):
-        filename = f"./long-term-st-database-1.0.0/s200{h}1"
+    prefix = "E:/Users/timotej999/Downloads/long-term-st-database-1.0.0/"
+    results = []
+    db = f"{prefix}./long-term-st-database-1.0.0/"
+    done = set()
+    for p in os.listdir(db):
+        if os.path.isdir(p):
+            continue
+        if os.path.exists(db + p.split("/")[-1].split(".")[0] + ".hea"):
+            filename = db + p.split("/")[-1].split(".")[0]
+        else:
+            continue
+        if filename.split("/")[-1] in done:
+            continue
+        # for h in range(1, 10):
+        #    filename = f"{prefix}./long-term-st-database-1.0.0/s200{h}1"
+        # filename = f"{prefix}./long-term-st-database-1.0.0/s20501"
         anns = wfdb.rdann(filename, "atr")
-        true_beats = anns.sample[anns.sample < 1250]
+        # true_beats = anns.sample[anns.sample < 1250]
         true_beats = anns.sample
         rec = wfdb.rdrecord(filename)
 
-        for i in range(1):
-            sig = rec.p_signal[:, i][:]  # 0:250 * 60 * 60]
-            #detect(sig, true_beats, search=True, name=filename.split("/")[-1], maxbeta=2.6, signum=i)
+        for i in range(2):
+            sig = rec.p_signal[:, i][:]  # [34*60*250:35*60*250]  # 0:250 * 60 * 60]
 
-            detect(sig, true_beats, T=0.15, beta1=1., beta2=1.)
-    #detect(sig, true_beats, T=0.68, beta1=0.6, beta2=1.8)
-    #print(f"T: {T}, b1: {b1}, b2: {b2}, max score: {maxs}")
-
-
-quit()
-cands = ...
-r_peaks = ...
-adapted_cands = ...
-# TODO baseline extraction??? kaj je to, what do i do
-# TODO MORE PREPROCESSING - we require accurate candidates
-candidates = np.arange(len(sig))[cands]
-final = cands
-plt.figure(figsize=(15, 5))
-plt.plot(r_peaks)
-# plt.plot(second_order_diff)
-# plt.plot(sig)
-for i in final:
-    plt.axvline(x=i, c="red")
-for i in adapted_cands:
-    plt.axvline(x=i, c="blue")
-plt.show()
+            # plt.plot(sig[34*60*250:35*60*250])
+            # plt.show()
+            search = False
+            sens, pluspred = detect(sig, true_beats, search=search, name=filename.split("/")[-1], T=0.3, beta1=0.9,
+                                    beta2=1.3, signum=i, verbose=True, Tlim=(0.0, 2.0), beta1lim=(0.0, 2.),
+                                    beta2lim=(0.0, 2.), plot=False, annotate=True)
+            if i == 0:
+                results.append([filename.split("/")[-1], sens, pluspred])
+            else:
+                results[-1] += [sens, pluspred]
+        done.add(filename.split("/")[-1])
+    with open("./det/all.txt", "w") as f:
+        print("FILENAME, s1 SENS, s1 +PRED, s2 SENS, s2 +PRED", file=f)
+        for i in results:
+            print(f"{','.join([str(x) for x in i])}", file=f)
+    # detect(sig, true_beats, T=0.68, beta1=0.6, beta2=1.8)
+    # print(f"T: {T}, b1: {b1}, b2: {b2}, max score: {maxs}")
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import json
-dicc = {}
-with open("heartbeat_search.json", "w") as f:
-    json.dump(dicc, f)
+import numpy as np
+stats = []
+with open("./det/all.txt", "r") as f:
+    f.readline()
+    for line in f:
+        tokens = line.split(",")[1:]
+        stats.append((float(tokens[0]) + float(tokens[2]), float(tokens[1]) + float(tokens[3])))
 
-with open("heartbeat_search.json", "rb") as f:
-    dicd = json.load(f)
-
-print(dicd)
+stats = np.array(stats) / 2
+print(np.mean(stats, axis=0))
+print(np.median(stats, axis=0))
